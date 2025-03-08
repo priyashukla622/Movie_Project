2b40ce0634aace8166d6076030a153f32d1b39a8
priya shukla <priyashukla22@navgurukul.org>
2025-03-08 15:58:24 +0530

    updated file

--- frontend/script.js
+++ frontend/script.js
@@ -3,11 +3,19 @@ let input = document.getElementById("input-box");
 let btn = document.getElementById("btn");
 const grid = document.querySelector(".grid");
 
-btn.addEventListener("click", searchMovie);
+let allMovies = [];
+fetch("https://movie-project-ko1b.onrender.com/movies")
+    .then(response => response.json())
+    .then(data => {
+        console.log("API Response:", data);
+        allMovies = data.getMovies;
+        showAllMovies(allMovies);
+    })
+    .catch(error => console.error("Error fetching movies:", error));
 
-function displayMovies(movies) {
+// dislay all movie
+function showAllMovies(movies) {
     grid.innerHTML = ""; 
-
     if (movies.length === 0) {
         grid.innerHTML = `<p class="para">No movies found for this year.</p>`;
         return;
@@ -15,39 +23,33 @@ function displayMovies(movies) {
     movies.forEach(movie => {
         let movieDiv = document.createElement("div");
         movieDiv.classList.add("box");
-
         movieDiv.innerHTML = `
             <img src="${movie.poster}" alt="${movie.title}" width="180">
             <h3>${movie.title}</h3>
-            <p>${movie.year}</p>
-            <p> ${movie.genre.join(", ")}</p>
+             <p>${movie.year} | ${movie.genre.join(", ")}</p>
            <p>⭐ <span class="rating">${movie.rating}</span></p>
 
         `;
-
         grid.appendChild(movieDiv);
     });
 }
-// display all movie
-let allMovies = [];
-fetch("https://movie-project-ko1b.onrender.com/movies")
-    .then(response => response.json())
-    .then(data => {
-        console.log("API Response:", data);
-        allMovies = data.getMovies;
-        displayMovies(allMovies);
-    })
-    .catch(error => console.error("Error fetching movies:", error));
    // search movie by year
 function searchMovie() {
-    let searchYear = input.value.trim(); 
-    if (searchYear === "") {
-        displayMovies(allMovies); 
+    let searchTitle = input.value.trim(); 
+    if (searchTitle === "") {
+        showAllMovies(allMovies); 
         return;
     }
-    let filteredMovies = allMovies.filter(movie => movie.year == searchYear); 
-    displayMovies(filteredMovies);
+    let filteredMovies = allMovies.filter(movie => movie.title == searchTitle); 
+    showAllMovies(filteredMovies);
 }
+btn.addEventListener("click", searchMovie);
+
+input.addEventListener("keydown", function(event) {
+    if (event.key === "Enter") {
+        searchMovie();
+    }
+});
 
 
 
